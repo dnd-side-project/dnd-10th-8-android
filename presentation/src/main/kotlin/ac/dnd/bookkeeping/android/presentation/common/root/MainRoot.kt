516eacdab736985c7929b3f82d71c3e51a0b7d36
jinuemong <jinwoo3661@naver.com>
2024-01-17 15:42:11 +0900

    [Feat]: NavHost의 각 화면을 담당하는 ScreenRoot 기능 구현

---
+++ presentation/src/main/kotlin/ac/dnd/bookkeeping/android/presentation/common/root/MainRoot.kt
@@ -0,0 +1,11 @@
+package ac.dnd.bookkeeping.android.presentation.common.root
+
+object MainRoot {
+
+    const val BOTTOM_FIRST = "firstScreen"
+    const val BOTTOM_SECOND = "secondScreen"
+    const val BOTTOM_THIRD = "thirdScreen"
+    const val BOTTOM_FOURTH = "fourthScreen"
+    const val BOTTOM_FIFTH = "fifthScreen"
+
+}
---
+++ presentation/src/main/kotlin/ac/dnd/bookkeeping/android/presentation/common/root/ScreenRoot.kt
@@ -0,0 +1,8 @@
+package ac.dnd.bookkeeping.android.presentation.common.root
+
+object ScreenRoot {
+
+    const val SPLASH = "splashScreen"
+    const val MAIN_GRAPH = "mainGraph"
+
+}
