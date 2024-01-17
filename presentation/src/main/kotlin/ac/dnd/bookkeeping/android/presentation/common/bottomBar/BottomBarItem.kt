afe58a2a2f4d8e7d71f5ea0a95178d4e12ee2d90
jinuemong <jinwoo3661@naver.com>
2024-01-18 00:34:18 +0900

    [Feat]: bottom item 템플릿 생성

--- presentation/src/main/kotlin/ac/dnd/bookkeeping/android/presentation/common/bottomBar/BottomBarItem.kt
+++ presentation/src/main/kotlin/ac/dnd/bookkeeping/android/presentation/common/bottomBar/BottomBarItem.kt
@@ -1,5 +1,7 @@
 package ac.dnd.bookkeeping.android.presentation.common.bottomBar
 
+import ac.dnd.bookkeeping.android.presentation.R
+import ac.dnd.bookkeeping.android.presentation.common.root.MainRoot
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 
@@ -8,9 +10,44 @@ sealed class BottomBarItem(
     @DrawableRes val drawableResId: Int,
     @StringRes val stringResId: Int
 ) {
+    data object BottomFirst :
+        BottomBarItem(
+            route = MainRoot.BOTTOM_FIRST,
+            drawableResId = R.drawable.bottom_sample,
+            stringResId = R.string.bottom_sample
+        )
 
+    data object BottomSecond :
+        BottomBarItem(
+            route = MainRoot.BOTTOM_SECOND,
+            drawableResId = R.drawable.bottom_sample,
+            stringResId = R.string.bottom_sample
+        )
+
+    data object BottomThird :
+        BottomBarItem(
+            route = MainRoot.BOTTOM_THIRD,
+            drawableResId = R.drawable.bottom_sample,
+            stringResId = R.string.bottom_sample
+        )
+
+    data object BottomFourth :
+        BottomBarItem(
+            route = MainRoot.BOTTOM_FOURTH,
+            drawableResId = R.drawable.bottom_sample,
+            stringResId = R.string.bottom_sample
+        )
+
+    data object BottomFifth :
+        BottomBarItem(
+            route = MainRoot.BOTTOM_FIFTH,
+            drawableResId = R.drawable.bottom_sample,
+            stringResId = R.string.bottom_sample
+        )
 
     companion object {
-        val BOTTOM_NAV_ITEMS = listOf<BottomBarItem>()
+        val BOTTOM_NAV_ITEMS = listOf<BottomBarItem>(
+            BottomFirst, BottomSecond, BottomThird, BottomFourth, BottomFifth
+        )
     }
 }
