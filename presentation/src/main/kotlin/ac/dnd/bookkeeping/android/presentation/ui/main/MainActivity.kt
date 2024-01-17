2d8992de77de3f02ce597d5af2a9c6349dff89b4
jinuemong <jinwoo3661@naver.com>
2024-01-17 18:55:56 +0900

    [Feat]: main ui 구현과 BottomView 적용

--- presentation/src/main/kotlin/ac/dnd/bookkeeping/android/presentation/ui/main/MainActivity.kt
+++ presentation/src/main/kotlin/ac/dnd/bookkeeping/android/presentation/ui/main/MainActivity.kt
@@ -1,8 +1,18 @@
 package ac.dnd.bookkeeping.android.presentation.ui.main
 
-import androidx.activity.viewModels
-import ac.dnd.bookkeeping.android.presentation.databinding.ActivityMainBinding
 import ac.dnd.bookkeeping.android.presentation.common.base.BaseActivity
+import ac.dnd.bookkeeping.android.presentation.common.navigation.MainNavGraph
+import ac.dnd.bookkeeping.android.presentation.common.state.ManageSystemUiState
+import ac.dnd.bookkeeping.android.presentation.common.state.rememberApplicationState
+import ac.dnd.bookkeeping.android.presentation.databinding.ActivityMainBinding
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -14,6 +24,27 @@ class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::infl
         bind {
             vm = viewModel
             lifecycleOwner = this@MainActivity
+            setContent {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = Color.White
+                ){
+                    MainUi()
+                }
+            }
         }
     }
+}
+
+@Composable
+fun MainUi() {
+    val appState = rememberApplicationState()
+    ManageSystemUiState(appState = appState)
+    MainNavGraph(appState = appState)
+}
+
+@Preview
+@Composable
+fun PreviewMainUi(){
+    MainUi()
 }
