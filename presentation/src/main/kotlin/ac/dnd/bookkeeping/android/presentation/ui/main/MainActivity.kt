45a0e99dabaafc7b557c6a1a2d2b9ac76bf7dbe8
jinuemong <jinwoo3661@naver.com>
2024-01-18 02:00:28 +0900

    [Feat]: Compose ui Theme 지정과 뷰 활성화

--- presentation/src/main/kotlin/ac/dnd/bookkeeping/android/presentation/ui/main/MainActivity.kt
+++ presentation/src/main/kotlin/ac/dnd/bookkeeping/android/presentation/ui/main/MainActivity.kt
@@ -5,13 +5,14 @@ import ac.dnd.bookkeeping.android.presentation.common.navigation.MainNavGraph
 import ac.dnd.bookkeeping.android.presentation.common.state.ManageSystemUiState
 import ac.dnd.bookkeeping.android.presentation.common.state.rememberApplicationState
 import ac.dnd.bookkeeping.android.presentation.databinding.ActivityMainBinding
+import ac.dnd.bookkeeping.android.presentation.ui.main.theme.BookkeepingTheme
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -24,27 +25,29 @@ class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::infl
         bind {
             vm = viewModel
             lifecycleOwner = this@MainActivity
-            setContent {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = Color.White
-                ){
-                    MainUi()
-                }
-            }
+        }
+        setContent {
+            MainUi()
         }
     }
 }
 
 @Composable
 fun MainUi() {
-    val appState = rememberApplicationState()
-    ManageSystemUiState(appState = appState)
-    MainNavGraph(appState = appState)
+    BookkeepingTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colors.background
+        ) {
+            val appState = rememberApplicationState()
+            ManageSystemUiState(appState = appState)
+            MainNavGraph(appState = appState)
+        }
+    }
 }
 
 @Preview
 @Composable
-fun PreviewMainUi(){
+fun PreviewMainUi() {
     MainUi()
 }
