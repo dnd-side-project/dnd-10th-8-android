bfe20f27ef780a842de53ef203b36d2b9aa35675
jinuemong <jinwoo3661@naver.com>
2024-01-19 01:44:03 +0900

    [Feat]: Login 플로우를 담당하는 LOGIN ScreenRootConstant 생성

--- presentation/src/main/kotlin/ac/dnd/bookkeeping/android/presentation/common/root/ScreenRootConstant.kt
+++ presentation/src/main/kotlin/ac/dnd/bookkeeping/android/presentation/common/root/ScreenRootConstant.kt
@@ -3,6 +3,7 @@ package ac.dnd.bookkeeping.android.presentation.common.root
 object ScreenRootConstant {
 
     const val SPLASH = "splashScreen"
+    const val LOGIN ="loginGraph"
     const val MAIN_GRAPH = "mainGraph"
 
 }
