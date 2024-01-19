90e9ec959fe9a9c00d32089b4b5b8b9e8822099e
jinuemong <jinwoo3661@naver.com>
2024-01-19 13:10:26 +0900

    [Refactor]: 각 그래프 별 SplashScreen 분기 처리

--- presentation/src/main/kotlin/ac/dnd/bookkeeping/android/presentation/common/root/ScreenRootConstant.kt
+++ presentation/src/main/kotlin/ac/dnd/bookkeeping/android/presentation/common/root/ScreenRootConstant.kt
@@ -2,7 +2,8 @@ package ac.dnd.bookkeeping.android.presentation.common.root
 
 object ScreenRootConstant {
 
-    const val SPLASH = "splashScreen"
+    const val LOGIN_SPLASH = "loginSplashScreen"
+    const val MAIN_SPLASH = "mainSplashScreen"
     const val LOGIN_GRAPH ="loginGraph"
     const val MAIN_GRAPH = "mainGraph"
 
