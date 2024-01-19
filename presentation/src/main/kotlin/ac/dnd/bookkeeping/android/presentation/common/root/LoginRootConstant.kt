ebca65b9bfacdfa9fc9071ad621cc0e300ae6f3b
jinuemong <jinwoo3661@naver.com>
2024-01-19 12:14:55 +0900

    [Feat]: 로그인 화면 이동 관련 LoginRootConstant 구현

---
+++ presentation/src/main/kotlin/ac/dnd/bookkeeping/android/presentation/common/root/LoginRootConstant.kt
@@ -0,0 +1,8 @@
+package ac.dnd.bookkeeping.android.presentation.common.root
+
+object LoginRootConstant {
+
+    const val LOGIN_MAIN = "loginMainScreen"
+    const val LOGIN_ONBOARD = "loginOnboardScreen"
+
+}
