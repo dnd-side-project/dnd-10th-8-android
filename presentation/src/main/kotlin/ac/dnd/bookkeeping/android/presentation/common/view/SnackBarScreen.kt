820c230918d5fe7b10a99da6cfc1e9594b8e547a
jinuemong <jinwoo3661@naver.com>
2024-01-21 14:12:28 +0900

    [Feat]: Compose 스낵바 기능 구현

---
+++ presentation/src/main/kotlin/ac/dnd/bookkeeping/android/presentation/common/view/SnackBarScreen.kt
@@ -0,0 +1,70 @@
+package ac.dnd.bookkeeping.android.presentation.common.view
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.SnackbarDuration
+import androidx.compose.material.SnackbarHost
+import androidx.compose.material.SnackbarHostState
+import androidx.compose.material.SnackbarResult
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.launch
+
+@Composable
+fun SnackBarScreen(
+    message: String,
+    onDismiss: () -> Unit,
+    onClick: () -> Unit
+) {
+    val scope = rememberCoroutineScope()
+    val snackBarState = remember { SnackbarHostState() }
+
+    LaunchedEffect(Unit) {
+        scope.launch {
+            val result = snackBarState.showSnackbar(
+                message = message,
+                actionLabel = "Action",
+                duration = SnackbarDuration.Short
+            )
+            when (result) {
+                SnackbarResult.ActionPerformed -> {
+                    onClick()
+                }
+
+                SnackbarResult.Dismissed -> {
+                    onDismiss()
+                }
+            }
+        }
+    }
+
+    Box(modifier = Modifier.fillMaxSize()) {
+        SnackbarHost(
+            hostState = snackBarState,
+            modifier = Modifier.align(Alignment.BottomCenter)
+        ) { snackBarData ->
+            Surface(
+                modifier = Modifier.padding(20.dp),
+                shape = RoundedCornerShape(10.dp),
+                color = Color.LightGray
+            ) {
+                Text(
+                    text = snackBarData.message,
+                    fontSize = 20.sp,
+                    color = Color.Black
+                )
+            }
+        }
+    }
+}
