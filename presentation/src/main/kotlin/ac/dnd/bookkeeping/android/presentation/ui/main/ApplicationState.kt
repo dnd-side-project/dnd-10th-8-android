b476d98e58f01987f4366d13b228c65ec61c8301
jinuemong <jinwoo3661@naver.com>
2024-01-21 18:03:26 +0900

    [Refactor]: ApplicationState에 SnackBar 기능 추가

--- presentation/src/main/kotlin/ac/dnd/bookkeeping/android/presentation/ui/main/ApplicationState.kt
+++ presentation/src/main/kotlin/ac/dnd/bookkeeping/android/presentation/ui/main/ApplicationState.kt
@@ -1,7 +1,9 @@
 package ac.dnd.bookkeeping.android.presentation.ui.main
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.Stable
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
@@ -11,19 +13,32 @@ import com.google.accompanist.systemuicontroller.rememberSystemUiController
 @Stable
 class ApplicationState(
     val navController: NavHostController,
-    val systemUiController: SystemUiController
-)
+    val systemUiController: SystemUiController,
+    val snackBarMessage: MutableState<String>
+){
+    fun getSnackBarMessage() = snackBarMessage.value
+
+    fun resetSnackBarMessage(){
+        snackBarMessage.value = ""
+    }
+
+    fun setSnackBarMessage(message: String){
+        snackBarMessage.value = message
+    }
+}
 
 @Composable
 fun rememberApplicationState(
     navController: NavHostController = rememberNavController(),
-    systemUiController: SystemUiController = rememberSystemUiController()
+    systemUiController: SystemUiController = rememberSystemUiController(),
+    snackBarMessage: MutableState<String> = remember{ mutableStateOf("") }
 ) = remember(
     navController,
     systemUiController
 ) {
     ApplicationState(
         navController = navController,
-        systemUiController = systemUiController
+        systemUiController = systemUiController,
+        snackBarMessage = snackBarMessage
     )
 }
