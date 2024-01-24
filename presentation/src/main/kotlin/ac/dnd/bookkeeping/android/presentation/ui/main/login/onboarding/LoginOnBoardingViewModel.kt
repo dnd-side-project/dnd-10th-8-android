95725d3760f77baa5a20abcd6375f4117b565113
jinuemong <jinwoo3661@naver.com>
2024-01-24 23:21:54 +0900

    [Feat]: OnBoardingViewModel 구현과 OnBoarding Destination 뷰 연결

---
+++ presentation/src/main/kotlin/ac/dnd/bookkeeping/android/presentation/ui/main/login/onboarding/LoginOnBoardingViewModel.kt
@@ -0,0 +1,37 @@
+package ac.dnd.bookkeeping.android.presentation.ui.main.login.onboarding
+
+import ac.dnd.bookkeeping.android.presentation.common.base.BaseViewModel
+import ac.dnd.bookkeeping.android.presentation.common.util.coroutine.event.EventFlow
+import ac.dnd.bookkeeping.android.presentation.common.util.coroutine.event.MutableEventFlow
+import ac.dnd.bookkeeping.android.presentation.common.util.coroutine.event.asEventFlow
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class LoginOnBoardingViewModel @Inject constructor(
+    private val savedStateHandle: SavedStateHandle,
+) : BaseViewModel() {
+
+    private val _state: MutableStateFlow<LoginOnBoardingState> =
+        MutableStateFlow(LoginOnBoardingState.Init)
+    val state: StateFlow<LoginOnBoardingState> = _state.asStateFlow()
+
+    private val _event: MutableEventFlow<LoginOnBoardingEvent> = MutableEventFlow()
+    val event: EventFlow<LoginOnBoardingEvent> = _event.asEventFlow()
+
+    fun onIntent(intent: LoginOnBoardingIntent) {
+        when (intent) {
+            is LoginOnBoardingIntent.GoToNextStep -> {
+                viewModelScope.launch {
+                    _event.emit(LoginOnBoardingEvent.OnClickNextStep)
+                }
+            }
+        }
+    }
+}
