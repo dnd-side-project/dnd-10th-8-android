436075c33be2f1912fbfb32b7fa0eb8807f58983
jinuemong <jinwoo3661@naver.com>
2024-01-25 14:25:12 +0900

    [Refactor]: RegistrationNaming Intent & Event 재생성

---
+++ presentation/src/main/kotlin/ac/dnd/bookkeeping/android/presentation/ui/main/registration/naming/RegistrationNamingIntent.kt
@@ -0,0 +1,5 @@
+package ac.dnd.bookkeeping.android.presentation.ui.main.registration.naming
+
+sealed interface RegistrationNamingIntent {
+    data object SubmitUserInput : RegistrationNamingIntent
+}
