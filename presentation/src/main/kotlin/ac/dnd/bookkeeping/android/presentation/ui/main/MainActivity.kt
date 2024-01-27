86b487a8af8ee0d2b0debfeb5c2c8e1efbe88684
jinuemong <jinwoo3661@naver.com>
2024-01-27 13:45:12 +0900

    [Fix]: start screen 초기화

--- presentation/src/main/kotlin/ac/dnd/bookkeeping/android/presentation/ui/main/MainActivity.kt
+++ presentation/src/main/kotlin/ac/dnd/bookkeeping/android/presentation/ui/main/MainActivity.kt
@@ -1,7 +1,6 @@
 package ac.dnd.bookkeeping.android.presentation.ui.main
 
 import ac.dnd.bookkeeping.android.presentation.common.base.BaseActivity
-import ac.dnd.bookkeeping.android.presentation.ui.main.kakao_login_sample.SampleScreen
 import androidx.activity.compose.setContent
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -9,8 +8,8 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : BaseActivity() {
     override fun initView() {
         setContent {
-//            MainScreen()
-            SampleScreen()
+            MainScreen()
+//            SampleScreen()
         }
     }
 }
