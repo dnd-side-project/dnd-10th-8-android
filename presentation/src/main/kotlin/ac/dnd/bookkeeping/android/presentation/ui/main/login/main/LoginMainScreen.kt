6b4efebb6a2657275c17a55b813232ff2fbb1881
jinuemong <jinwoo3661@naver.com>
2024-01-28 01:05:19 +0900

    [Feat]: LoginScreen ui 구체화

--- presentation/src/main/kotlin/ac/dnd/bookkeeping/android/presentation/ui/main/login/main/LoginMainScreen.kt
+++ presentation/src/main/kotlin/ac/dnd/bookkeeping/android/presentation/ui/main/login/main/LoginMainScreen.kt
@@ -1,5 +1,6 @@
 package ac.dnd.bookkeeping.android.presentation.ui.main.login.main
 
+import ac.dnd.bookkeeping.android.presentation.R
 import ac.dnd.bookkeeping.android.presentation.common.util.LaunchedEffectWithLifecycle
 import ac.dnd.bookkeeping.android.presentation.common.util.coroutine.event.EventFlow
 import ac.dnd.bookkeeping.android.presentation.common.util.coroutine.event.MutableEventFlow
@@ -8,19 +9,27 @@ import ac.dnd.bookkeeping.android.presentation.ui.main.ApplicationState
 import ac.dnd.bookkeeping.android.presentation.ui.main.home.HomeConstant
 import ac.dnd.bookkeeping.android.presentation.ui.main.login.LoginConstant
 import ac.dnd.bookkeeping.android.presentation.ui.main.rememberApplicationState
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -62,32 +71,38 @@ fun LoginMainScreen(
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .background(color = Color.Black),
+            .background(color = Color.White)
     ) {
         Text(
-            text = "OnBoard Screen",
-            fontSize = 20.sp,
-            color = Color.White,
-            modifier = Modifier.align(Alignment.Center)
+            text = "서비스 이름",
+            fontWeight = FontWeight.Bold,
+            fontSize = 30.sp,
+            color = Color(0xFF474747),
+            modifier = Modifier
+                .align(Alignment.TopCenter)
+                .padding(top = 157.16.dp)
         )
 
-        Surface(
+        Column(
             modifier = Modifier
                 .align(Alignment.BottomCenter)
-                .padding(30.dp)
+                .padding(
+                    bottom = 77.dp,
+                    start = 18.dp,
+                    end = 18.dp
+                )
                 .fillMaxWidth()
                 .clickable {
                     navigateToLogin()
-                },
-            shape = RoundedCornerShape(10.dp),
-            color = Color.White
+                }
+                .clip(RoundedCornerShape(10.dp)),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(
-                text = "next",
-                fontSize = 20.sp,
-                color = Color.Black,
-                textAlign = TextAlign.Center,
-                modifier = Modifier.padding(5.dp)
+            SampleComponent()
+            Spacer(Modifier.height(20.23.dp))
+            Image(
+                painter = painterResource(R.drawable.ic_kakao_login_button),
+                contentDescription = null
             )
         }
     }
@@ -98,6 +113,33 @@ fun LoginMainScreen(
                 is LoginMainEvent.Login -> login(event)
             }
         }
+    }
+}
+
+@Composable
+private fun SampleComponent(){
+    Box(
+        modifier = Modifier
+            .width(115.96.dp)
+            .height(32.47.dp)
+            .shadow(
+                elevation = 3.dp,
+                shape = RoundedCornerShape(16.23.dp)
+            )
+            .background(
+                shape = RoundedCornerShape(16.23.dp),
+                color = Color.White
+            ),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "3초만에 시작하기",
+            color = Color.Black,
+            fontSize = 12.sp,
+            fontWeight = FontWeight.Bold,
+            letterSpacing = 0.2.sp,
+            textAlign = TextAlign.Center
+        )
     }
 }
 
