2db3865d1f652cd114b776802d3281de768bd68f
jinuemong <jinwoo3661@naver.com>
2024-02-01 19:41:57 +0900

    [Feat]: History 관련 초기 기능 구현

---
+++ presentation/src/main/kotlin/ac/dnd/bookkeeping/android/presentation/ui/main/home/history/detail/HistoryDetailEvent.kt
@@ -0,0 +1,4 @@
+package ac.dnd.bookkeeping.android.presentation.ui.main.home.history.detail
+
+class HistoryDetailEvent {
+}
---
+++ presentation/src/main/kotlin/ac/dnd/bookkeeping/android/presentation/ui/main/home/history/detail/HistoryDetailModel.kt
@@ -0,0 +1,8 @@
+package ac.dnd.bookkeeping.android.presentation.ui.main.home.history.detail
+
+import androidx.compose.runtime.Immutable
+
+@Immutable
+data class HistoryDetailModel(
+    val state: HistoryDetailState
+)
