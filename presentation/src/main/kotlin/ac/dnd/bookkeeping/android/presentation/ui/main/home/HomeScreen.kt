75af06913b2f03ccf0550f229e6f44b6325f013a
jinuemong <jinwoo3661@naver.com>
2024-02-02 13:36:28 +0900

    [Fix]: HistoryScreen 제거

--- presentation/src/main/kotlin/ac/dnd/bookkeeping/android/presentation/ui/main/home/HomeScreen.kt
+++ presentation/src/main/kotlin/ac/dnd/bookkeeping/android/presentation/ui/main/home/HomeScreen.kt
@@ -4,7 +4,6 @@ import ac.dnd.bookkeeping.android.presentation.common.util.ErrorObserver
 import ac.dnd.bookkeeping.android.presentation.common.view.CustomSnackBarHost
 import ac.dnd.bookkeeping.android.presentation.ui.main.ApplicationState
 import ac.dnd.bookkeeping.android.presentation.ui.main.home.bookkeeping.BookkeepingScreen
-import ac.dnd.bookkeeping.android.presentation.ui.main.home.history.HistoryScreen
 import ac.dnd.bookkeeping.android.presentation.ui.main.home.setting.SettingScreen
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.fillMaxSize
