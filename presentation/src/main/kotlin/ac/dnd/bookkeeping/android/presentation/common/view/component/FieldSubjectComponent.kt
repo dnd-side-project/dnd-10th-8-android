630258b46a136bce28621d1510f93d418c01a6f5
jinuemong <jinwoo3661@naver.com>
2024-02-03 21:00:08 +0900

    [Refactor]: 제목 필드 재사용 가능하도록 컴포넌트화

---
+++ presentation/src/main/kotlin/ac/dnd/bookkeeping/android/presentation/common/view/component/FieldSubjectComponent.kt
@@ -0,0 +1,52 @@
+package ac.dnd.bookkeeping.android.presentation.common.view.component
+
+import ac.dnd.bookkeeping.android.presentation.R
+import ac.dnd.bookkeeping.android.presentation.common.theme.Body1
+import ac.dnd.bookkeeping.android.presentation.common.theme.Gray700
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun FieldSubject(
+    subject: String
+) {
+    Column {
+        Row {
+            Text(
+                text = subject,
+                style = Body1.merge(
+                    color = Gray700,
+                    fontWeight = FontWeight.SemiBold
+                )
+            )
+            Spacer(modifier = Modifier.width(1.dp))
+            Box(
+                modifier = Modifier
+                    .height(21.dp)
+                    .padding(bottom = 3.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.ic_essential_field),
+                    contentDescription = null
+                )
+            }
+        }
+        Spacer(
+            modifier = Modifier.height(18.dp)
+        )
+    }
+}
