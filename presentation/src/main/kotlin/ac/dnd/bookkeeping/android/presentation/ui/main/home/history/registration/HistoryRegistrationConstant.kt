3a703e3dcf0d34f99d32a6809a2c157f7a4b9168
jinuemong <jinwoo3661@naver.com>
2024-02-03 17:20:00 +0900

    [Feat]: HistoryRegistration 관련 interface 생성

---
+++ presentation/src/main/kotlin/ac/dnd/bookkeeping/android/presentation/ui/main/home/history/registration/HistoryRegistrationConstant.kt
@@ -0,0 +1,7 @@
+package ac.dnd.bookkeeping.android.presentation.ui.main.home.history.registration
+
+import ac.dnd.bookkeeping.android.presentation.ui.main.home.history.HistoryConstant
+
+object HistoryRegistrationConstant {
+    const val ROUTE: String = "${HistoryConstant.ROUTE}/registration"
+}
