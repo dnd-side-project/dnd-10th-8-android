50ae5dac5ab729f6586f0500eb4ec4b107f9f82e
jinuemong <jinwoo3661@naver.com>
2024-02-04 14:58:59 +0900

    [Feat]: History Registration 관련 타입 지정

---
+++ presentation/src/main/kotlin/ac/dnd/bookkeeping/android/presentation/ui/main/home/history/registration/type/RegistrationTagType.kt
@@ -0,0 +1,20 @@
+package ac.dnd.bookkeeping.android.presentation.ui.main.home.history.registration.type
+
+enum class RegistrationTagType(
+    val id: Long,
+    val tagName: String
+) {
+    ATTEND(0,"참석"),
+    ACCOUNT(1,"계좌"),
+    GIFT_IN_RETURN(2,"답례품"),
+    CASH(3, "현금"),
+    NONAPPEARANCE(4,"불참"),
+    GIFT_CARD(5,"상품권"),
+    WREATH(6,"화환");
+
+    companion object {
+        fun getTagName(id:Long): String {
+            return entries.find {it.id == id}?.tagName ?: ""
+        }
+    }
+}
---
+++ presentation/src/main/kotlin/ac/dnd/bookkeeping/android/presentation/ui/main/home/history/registration/type/RegistrationType.kt
@@ -0,0 +1,8 @@
+package ac.dnd.bookkeeping.android.presentation.ui.main.home.history.registration.type
+
+enum class RegistrationType(
+    val typeName: String
+) {
+    TAKE("받은 마음"),
+    GIVE("보낸 마음")
+}
