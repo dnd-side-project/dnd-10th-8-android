ea577c4960e594760954af459525f9dc146cb328
jinuemong <jinwoo3661@naver.com>
2024-02-08 03:10:28 +0900

    [Fix]: Snackbar ui 수정

--- presentation/src/main/kotlin/ac/dnd/bookkeeping/android/presentation/common/view/SnackBarScreen.kt
+++ presentation/src/main/kotlin/ac/dnd/bookkeeping/android/presentation/common/view/SnackBarScreen.kt
@@ -1,16 +1,18 @@
 package ac.dnd.bookkeeping.android.presentation.common.view
 
+import ac.dnd.bookkeeping.android.presentation.common.theme.Body1
+import ac.dnd.bookkeeping.android.presentation.common.theme.Gray100
+import ac.dnd.bookkeeping.android.presentation.common.theme.Gray800
+import ac.dnd.bookkeeping.android.presentation.common.theme.Shapes
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 
 @Composable
 fun SnackBarScreen(message: String) {
@@ -19,15 +21,22 @@ fun SnackBarScreen(message: String) {
             .padding(20.dp)
             .fillMaxWidth()
             .background(
-                shape = RoundedCornerShape(10.dp),
-                color = Color.LightGray
+                shape = Shapes.medium,
+                color = Gray800
             ),
     ) {
         Text(
             text = message,
-            fontSize = 20.sp,
-            color = Color.White,
-            modifier = Modifier.padding(20.dp),
+            style = Body1.merge(
+                color = Gray100,
+                fontWeight = FontWeight.Normal
+            ),
+            modifier = Modifier
+                .padding(
+                    start = 16.dp,
+                    end = 8.dp
+                )
+                .padding(vertical = 14.dp)
         )
     }
 }
