bc7b4439b38d417f1580c929aaa57e1fab05dda3
jinuemong <jinwoo3661@naver.com>
2024-02-08 17:37:43 +0900

    [Feat]: 로직 구현

--- presentation/src/main/kotlin/ac/dnd/bookkeeping/android/presentation/ui/main/home/history/detail/HistoryDetailIntent.kt
+++ presentation/src/main/kotlin/ac/dnd/bookkeeping/android/presentation/ui/main/home/history/detail/HistoryDetailIntent.kt
@@ -1,3 +1,7 @@
 package ac.dnd.bookkeeping.android.presentation.ui.main.home.history.detail
 
-sealed interface HistoryDetailIntent
+import ac.dnd.bookkeeping.android.presentation.model.history.HistoryViewType
+
+sealed interface HistoryDetailIntent {
+    data class ClickTab(val type: HistoryViewType) : HistoryDetailIntent
+}
--- presentation/src/main/kotlin/ac/dnd/bookkeeping/android/presentation/ui/main/home/history/detail/HistoryDetailModel.kt
+++ presentation/src/main/kotlin/ac/dnd/bookkeeping/android/presentation/ui/main/home/history/detail/HistoryDetailModel.kt
@@ -1,5 +1,8 @@
 package ac.dnd.bookkeeping.android.presentation.ui.main.home.history.detail
 
+import ac.dnd.bookkeeping.android.domain.model.feature.relation.RelationDetailWithUserInfo
+
 data class HistoryDetailModel(
     val state: HistoryDetailState,
+    val relationDetail: RelationDetailWithUserInfo
 )
