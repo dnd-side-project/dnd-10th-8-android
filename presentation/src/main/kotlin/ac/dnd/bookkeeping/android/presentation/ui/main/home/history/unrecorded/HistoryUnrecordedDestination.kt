a970ddbee6828a6ca220acff774c800501052629
Ray Jang <ajou4095@gmail.com>
2024-02-12 13:16:00 +0900

    [Feature] Schedule API 업데이트

--- presentation/src/main/kotlin/ac/dnd/bookkeeping/android/presentation/ui/main/home/history/unrecorded/HistoryUnrecordedDestination.kt
+++ presentation/src/main/kotlin/ac/dnd/bookkeeping/android/presentation/ui/main/home/history/unrecorded/HistoryUnrecordedDestination.kt
@@ -2,6 +2,7 @@ package ac.dnd.bookkeeping.android.presentation.ui.main.home.history.unrecorded
 
 import ac.dnd.bookkeeping.android.domain.model.feature.schedule.UnrecordedSchedule
 import ac.dnd.bookkeeping.android.domain.model.feature.schedule.UnrecordedScheduleRelation
+import ac.dnd.bookkeeping.android.domain.model.feature.schedule.UnrecordedScheduleRelationGroup
 import ac.dnd.bookkeeping.android.presentation.common.util.ErrorObserver
 import ac.dnd.bookkeeping.android.presentation.ui.main.ApplicationState
 import androidx.compose.runtime.getValue
@@ -26,13 +27,13 @@ fun NavGraphBuilder.historyUnrecordedDestination(
                 UnrecordedScheduleRelation(
                     0,
                     "친구",
+                    group = UnrecordedScheduleRelationGroup(
+                        0,
+                        "그룹"
+                    )
                 ),
                 day = LocalDate(2024, 1, 1),
                 event = "결혼",
-                alarm = LocalDateTime(
-                    LocalDate(2024, 2, 2),
-                    LocalTime(12, 12)
-                ),
                 time = LocalTime(12, 12),
                 link = "",
                 location = ""
@@ -42,13 +43,13 @@ fun NavGraphBuilder.historyUnrecordedDestination(
                 UnrecordedScheduleRelation(
                     0,
                     "가족",
+                    group = UnrecordedScheduleRelationGroup(
+                        0,
+                        "그룹"
+                    )
                 ),
                 day = LocalDate(2024, 2, 2),
                 event = "돌잔치",
-                alarm = LocalDateTime(
-                    LocalDate(2024, 2, 2),
-                    LocalTime(12, 12)
-                ),
                 time = LocalTime(12, 12),
                 link = "",
                 location = ""
@@ -58,13 +59,13 @@ fun NavGraphBuilder.historyUnrecordedDestination(
                 UnrecordedScheduleRelation(
                     0,
                     "친구",
+                    group = UnrecordedScheduleRelationGroup(
+                        0,
+                        "그룹"
+                    )
                 ),
                 day = LocalDate(2024, 3, 3),
                 event = "출산",
-                alarm = LocalDateTime(
-                    LocalDate(2024, 2, 2),
-                    LocalTime(12, 12)
-                ),
                 time = LocalTime(12, 12),
                 link = "",
                 location = ""
@@ -74,13 +75,13 @@ fun NavGraphBuilder.historyUnrecordedDestination(
                 UnrecordedScheduleRelation(
                     0,
                     "친구",
+                    group = UnrecordedScheduleRelationGroup(
+                        0,
+                        "그룹"
+                    )
                 ),
                 day = LocalDate(2024, 4, 4),
                 event = "생일",
-                alarm = LocalDateTime(
-                    LocalDate(2024, 2, 2),
-                    LocalTime(12, 12)
-                ),
                 time = LocalTime(12, 12),
                 link = "",
                 location = ""
@@ -90,13 +91,13 @@ fun NavGraphBuilder.historyUnrecordedDestination(
                 UnrecordedScheduleRelation(
                     0,
                     "지인",
+                    group = UnrecordedScheduleRelationGroup(
+                        0,
+                        "그룹"
+                    )
                 ),
                 day = LocalDate(2024, 5, 5),
                 event = "결혼",
-                alarm = LocalDateTime(
-                    LocalDate(2024, 2, 2),
-                    LocalTime(12, 12)
-                ),
                 time = LocalTime(12, 12),
                 link = "",
                 location = ""
@@ -106,13 +107,13 @@ fun NavGraphBuilder.historyUnrecordedDestination(
                 UnrecordedScheduleRelation(
                     0,
                     "가족",
+                    group = UnrecordedScheduleRelationGroup(
+                        0,
+                        "그룹"
+                    )
                 ),
                 day = LocalDate(2024, 6, 6),
                 event = "기타",
-                alarm = LocalDateTime(
-                    LocalDate(2024, 2, 2),
-                    LocalTime(12, 12)
-                ),
                 time = LocalTime(12, 12),
                 link = "",
                 location = ""
