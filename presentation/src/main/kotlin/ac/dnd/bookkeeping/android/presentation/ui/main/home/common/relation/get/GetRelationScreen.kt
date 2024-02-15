45d60525422791df18824e3927333c92e62267ee
jinuemong <jinwoo3661@naver.com>
2024-02-15 23:44:53 +0900

    [Feat]: 이름 선택 구체화

--- presentation/src/main/kotlin/ac/dnd/bookkeeping/android/presentation/ui/main/home/common/relation/get/GetRelationScreen.kt
+++ presentation/src/main/kotlin/ac/dnd/bookkeeping/android/presentation/ui/main/home/common/relation/get/GetRelationScreen.kt
@@ -8,10 +8,11 @@ import ac.dnd.bookkeeping.android.presentation.common.theme.Body1
 import ac.dnd.bookkeeping.android.presentation.common.theme.Gray000
 import ac.dnd.bookkeeping.android.presentation.common.theme.Gray200
 import ac.dnd.bookkeeping.android.presentation.common.theme.Gray500
+import ac.dnd.bookkeeping.android.presentation.common.theme.Gray600
 import ac.dnd.bookkeeping.android.presentation.common.theme.Gray700
+import ac.dnd.bookkeeping.android.presentation.common.theme.Gray800
 import ac.dnd.bookkeeping.android.presentation.common.theme.Headline2
 import ac.dnd.bookkeeping.android.presentation.common.theme.Headline3
-import ac.dnd.bookkeeping.android.presentation.common.theme.Primary4
 import ac.dnd.bookkeeping.android.presentation.common.theme.Shapes
 import ac.dnd.bookkeeping.android.presentation.common.util.ErrorObserver
 import ac.dnd.bookkeeping.android.presentation.common.util.LaunchedEffectWithLifecycle
@@ -19,17 +20,23 @@ import ac.dnd.bookkeeping.android.presentation.common.util.coroutine.event.Event
 import ac.dnd.bookkeeping.android.presentation.common.util.coroutine.event.MutableEventFlow
 import ac.dnd.bookkeeping.android.presentation.common.util.coroutine.event.eventObserve
 import ac.dnd.bookkeeping.android.presentation.common.view.BottomSheetScreen
-import ac.dnd.bookkeeping.android.presentation.common.view.CustomTextField
 import ac.dnd.bookkeeping.android.presentation.common.view.confirm.ConfirmButton
 import ac.dnd.bookkeeping.android.presentation.common.view.confirm.ConfirmButtonProperties
 import ac.dnd.bookkeeping.android.presentation.common.view.confirm.ConfirmButtonSize
 import ac.dnd.bookkeeping.android.presentation.common.view.confirm.ConfirmButtonType
+import ac.dnd.bookkeeping.android.presentation.common.view.textfield.TypingTextField
+import ac.dnd.bookkeeping.android.presentation.common.view.textfield.TypingTextFieldType
 import ac.dnd.bookkeeping.android.presentation.ui.main.ApplicationState
+import ac.dnd.bookkeeping.android.presentation.ui.main.home.common.relation.RelationConstant
 import ac.dnd.bookkeeping.android.presentation.ui.main.rememberApplicationState
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -40,11 +47,10 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Card
 import androidx.compose.material.Divider
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Icon
-import androidx.compose.material.RadioButton
-import androidx.compose.material.RadioButtonDefaults
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -54,8 +60,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -94,7 +103,6 @@ fun SearchRelationScreen(
 }
 
 @Composable
-@OptIn(ExperimentalMaterialApi::class)
 private fun SearchRelationScreen(
     appState: ApplicationState,
     model: GetRelationModel,
@@ -106,7 +114,7 @@ private fun SearchRelationScreen(
 ) {
     var text by remember { mutableStateOf("") }
     var selectedRelation: RelationSimple? by remember { mutableStateOf(null) }
-
+    var isTextFieldFocus by remember { mutableStateOf(false) }
     val lowerText = text.lowercase()
     val filteredGroups = model.groups.filter { group ->
         group.relationList.any { relation ->
@@ -115,8 +123,8 @@ private fun SearchRelationScreen(
     }
 
     fun navigateToEditRelation() {
-        // TODO : EmptyCase
-//        appState.navController.navigate(EditRelationConstant.ROUTE)
+        onDismissRequest()
+        appState.navController.navigate(RelationConstant.ROUTE)
     }
 
     BottomSheetScreen(
@@ -132,45 +140,89 @@ private fun SearchRelationScreen(
             modifier = Modifier
                 .wrapContentHeight()
                 .background(Gray200)
-                .padding(20.dp),
-            horizontalAlignment = Alignment.Start,
+                .padding(horizontal = 20.dp),
             verticalArrangement = Arrangement.Center
         ) {
-            Spacer(modifier = Modifier.height(16.dp))
+            Spacer(modifier = Modifier.height(20.dp))
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.ic_close),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .align(Alignment.CenterEnd)
+                        .clickable {
+                            onDismissRequest()
+                        }
+                )
+            }
             Text(
                 text = "이름 선택",
-                style = Headline2
+                style = Headline2.merge(
+                    color = Gray800,
+                    fontWeight = FontWeight.SemiBold
+                ),
+                modifier = Modifier.padding(vertical = 8.dp)
             )
-            Spacer(modifier = Modifier.height(8.dp))
             Text(
                 text = "이전에 등록한 관계에서 찾아보세요.",
-                style = Headline2
+                style = Headline2.merge(
+                    color = Gray700,
+                    fontWeight = FontWeight.Normal
+                )
             )
-            Spacer(modifier = Modifier.height(20.dp))
-            CustomTextField(
+            Spacer(modifier = Modifier.height(12.dp))
+            TypingTextField(
+                textType = TypingTextFieldType.Basic,
                 text = text,
-                height = 34.dp,
-                isSingleLine = true,
-                textStyle = Body1,
-                backgroundColor = Gray200,
-                elevation = 0.dp,
-                onTextChange = {
+                onValueChange = {
                     text = it
                 },
-                hintTextContent = {
-                    Text(
-                        text = "검색어를 입력하세요.",
-                        style = Body1.merge(color = Gray700)
-                    )
-                },
+                fieldHeight = 39.dp,
+                isSingleLine = true,
+                textStyle = Body1.merge(
+                    color = Gray700,
+                    fontWeight = FontWeight.Normal
+                ),
+                contentPadding = PaddingValues(
+                    start = if (!isTextFieldFocus && text.isEmpty()) 6.dp else 16.dp,
+                ),
+                backgroundColor = Gray000,
+                hintText = "이름을 입력하세요.",
+                hintTextColor = Gray500,
+                basicBorderColor = Color.Transparent,
                 leadingIconContent = {
-                    Icon(
-                        painter = painterResource(id = R.drawable.ic_search),
-                        contentDescription = null
-                    )
+                    if (!isTextFieldFocus && text.isEmpty()) {
+                        Box(modifier = Modifier.padding(start = 16.dp)) {
+                            Icon(
+                                painter = painterResource(id = R.drawable.ic_search),
+                                contentDescription = null,
+                                modifier = Modifier.size(16.dp)
+                            )
+                        }
+                    }
+                },
+                trailingIconContent = if (text.isNotEmpty()) {
+                    {
+                        Image(
+                            painter = painterResource(R.drawable.ic_close_circle),
+                            contentDescription = null,
+                            modifier = Modifier
+                                .size(20.dp)
+                                .clickable {
+                                    text = ""
+                                }
+                        )
+                    }
+                } else null,
+                onTextFieldFocusChange = {
+                    isTextFieldFocus = it
                 }
             )
-            Spacer(modifier = Modifier.height(16.dp))
+            Spacer(modifier = Modifier.height(32.dp))
             LazyColumn {
                 filteredGroups.forEach { group ->
                     item {
@@ -189,11 +241,42 @@ private fun SearchRelationScreen(
                     }
                 }
             }
+            Spacer(modifier = Modifier.height(16.dp))
 
-            Spacer(modifier = Modifier.height(10.dp))
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .clickable {
+                        navigateToEditRelation()
+                    }
+                    .padding(
+                        start = 2.dp,
+                        top = 16.dp,
+                        bottom = 16.dp
+                    )
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.ic_circle_plus),
+                    contentDescription = null
+                )
+                Spacer(modifier = Modifier.width(6.dp))
+                Text(
+                    text = "관계 등록하기",
+                    style = Headline3.merge(
+                        color = Gray600,
+                        fontWeight = FontWeight.SemiBold
+                    )
+                )
+            }
+            Spacer(modifier = Modifier.height(12.dp))
 
             ConfirmButton(
-                modifier = Modifier.fillMaxWidth(),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(
+                        horizontal = 20.dp,
+                        vertical = 12.dp
+                    ),
                 properties = ConfirmButtonProperties(
                     size = ConfirmButtonSize.Xlarge,
                     type = ConfirmButtonType.Primary
@@ -212,7 +295,6 @@ private fun SearchRelationScreen(
                     style = style
                 )
             }
-            Spacer(modifier = Modifier.height(10.dp))
         }
     }
 
@@ -235,16 +317,17 @@ private fun SearchRelationGroup(
     Column(
         modifier = Modifier.fillMaxSize()
     ) {
-        Surface(
+        Card(
             modifier = Modifier
                 .fillMaxWidth()
                 .height(56.dp),
             shape = Shapes.large,
             border = BorderStroke(1.dp, Gray500),
-            color = Color.White,
+            backgroundColor = Color.White,
             onClick = {
                 onExpandRequest()
-            }
+            },
+            elevation = if (isExpanded) 10.dp else 0.dp
         ) {
             Row(
                 horizontalArrangement = Arrangement.Center,
@@ -253,8 +336,12 @@ private fun SearchRelationGroup(
                 Spacer(modifier = Modifier.width(20.dp))
                 Text(
                     text = group.name,
-                    style = Headline3
+                    style = Headline3.merge(
+                        color = Gray800,
+                        fontWeight = FontWeight.SemiBold
+                    )
                 )
+                Spacer(modifier = Modifier.width(2.dp))
                 Icon(
                     painter = painterResource(id = R.drawable.ic_person),
                     contentDescription = null
@@ -262,17 +349,22 @@ private fun SearchRelationGroup(
                 Text(
                     modifier = Modifier.weight(1f),
                     text = group.relationList.size.toString(),
-                    style = Headline3.merge(Gray700)
+                    style = Headline3.merge(Gray600)
                 )
                 if (isExpanded) {
-                    Icon(
-                        painter = painterResource(id = R.drawable.ic_chevron_up),
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_drop_down),
+                        modifier = Modifier
+                            .rotate(180f)
+                            .size(24.dp),
                         contentDescription = null
                     )
                 } else {
-                    Icon(
-                        painter = painterResource(id = R.drawable.ic_chevron_down),
-                        contentDescription = null
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_drop_down),
+                        modifier = Modifier.size(24.dp),
+                        contentDescription = null,
+                        colorFilter = ColorFilter.tint(Gray500)
                     )
                 }
                 Spacer(modifier = Modifier.width(20.dp))
@@ -281,10 +373,11 @@ private fun SearchRelationGroup(
 
         if (isExpanded) {
             Spacer(modifier = Modifier.height(6.dp))
-            Surface(
+            Card(
                 border = BorderStroke(1.dp, Gray500),
-                color = Gray000,
-                shape = Shapes.large
+                backgroundColor = Gray000,
+                shape = Shapes.large,
+                elevation = 10.dp
             ) {
                 Column {
                     group.relationList.forEachIndexed { index, relation ->
@@ -323,7 +416,10 @@ private fun SearchRelationRelation(
             Spacer(modifier = Modifier.width(20.dp))
             Text(
                 text = relation.name,
-                style = Headline3
+                style = Headline3.merge(
+                    color = Gray800,
+                    fontWeight = FontWeight.SemiBold
+                )
             )
             Spacer(modifier = Modifier.width(3.dp))
             Text(
@@ -334,16 +430,23 @@ private fun SearchRelationRelation(
             Text(
                 modifier = Modifier.weight(1f),
                 text = relation.group.name,
-                style = Body0
-            )
-            RadioButton(
-                modifier = Modifier.size(24.dp),
-                selected = relation == selectedRelation,
-                onClick = { onClick(relation) },
-                colors = RadioButtonDefaults.colors(
-                    selectedColor = Primary4,
-                    unselectedColor = Gray500
+                style = Body0.merge(
+                    color = Gray700,
+                    fontWeight = FontWeight.Normal
                 )
+            )
+            Image(
+                modifier = Modifier
+                    .size(24.dp)
+                    .clickable {
+                        onClick(relation)
+                    },
+                painter = if (relation == selectedRelation) {
+                    painterResource(R.drawable.ic_check_circle)
+                } else {
+                    painterResource(R.drawable.ic_check_circle_gray)
+                },
+                contentDescription = null
             )
             Spacer(modifier = Modifier.width(20.dp))
         }
