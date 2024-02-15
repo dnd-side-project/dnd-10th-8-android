089fdd9bf170d3d69dfebcf0074f7eea59f0af3a
Ray Jang <ajou4095@gmail.com>
2024-02-15 17:36:09 +0900

    [Feature] 알람 구현

--- presentation/src/main/kotlin/ac/dnd/bookkeeping/android/presentation/common/util/alarm/AlarmUtil.kt
+++ presentation/src/main/kotlin/ac/dnd/bookkeeping/android/presentation/common/util/alarm/AlarmUtil.kt
@@ -8,7 +8,9 @@ import android.content.Intent
 import android.os.Build
 import kotlinx.datetime.Clock
 import kotlinx.datetime.TimeZone
+import kotlinx.datetime.minus
 import kotlinx.datetime.toInstant
+import kotlinx.datetime.todayIn
 
 fun registerAlarm(
     context: Context,
@@ -47,6 +49,8 @@ fun unregisterAlarm(
 }
 
 private fun Alarm.toIntent(context: Context): PendingIntent {
+    val now = Clock.System.todayIn(TimeZone.currentSystemDefault())
+
     val formattedTime = time?.let {
         val fixedHour = if (it.hour == 0) 24 else it.hour
         val timeHour = (fixedHour - 1) % 12 + 1
@@ -58,8 +62,22 @@ private fun Alarm.toIntent(context: Context): PendingIntent {
         }.getOrDefault("시간 없음")
     } ?: "시간 없음"
 
+    val formattedDate = (day - now).days.let {
+        when (it) {
+            0 -> "오늘"
+            1 -> "내일"
+            2 -> "모레"
+            in 3..6 -> "${it}일 후"
+            else -> "${it / 7}주 후"
+        }
+    }
+
     val title = "경사 미리알림"
-    val content = "내일 ${formattedTime}에 ${relation.name}님의 ${event} 일정이 있습니다."
+    val content = if (time == null) {
+        "$formattedDate ${relation.name}님의 $event 일정이 있습니다."
+    } else {
+        "$formattedDate ${formattedTime}에 ${relation.name}님의 $event 일정이 있습니다."
+    }
 
     return Intent(context, AlarmReceiver::class.java).apply {
         putExtra(AlarmReceiver.NOTIFICATION_ID, id.toInt())
