b2e8300655c80a44612cae3f17c3ea6889504327
Ray Jang <ajou4095@gmail.com>
2024-02-18 00:00:28 +0900

    [Feature] 로그인 시 Sentry User 정보 설정

--- domain/src/main/kotlin/ac/dnd/bookkeeping/android/domain/model/member/Profile.kt
+++ domain/src/main/kotlin/ac/dnd/bookkeeping/android/domain/model/member/Profile.kt
@@ -3,6 +3,9 @@ package ac.dnd.bookkeeping.android.domain.model.member
 import kotlinx.datetime.LocalDate
 
 data class Profile(
+    val id: Long,
+    val email: String,
+    val profileImageUrl: String,
     val name: String,
     val nickname: String,
     val gender: String,
