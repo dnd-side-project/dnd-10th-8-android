550318a726d1c114666292ba6c0a44725e807174
Ray Jang <ajou4095@gmail.com>
2024-02-20 18:56:00 +0900

    [System] Invalid Token 체크

--- app/src/main/kotlin/ac/dnd/mour/android/MourApplication.kt
+++ app/src/main/kotlin/ac/dnd/mour/android/MourApplication.kt
@@ -1,21 +1,44 @@
 package ac.dnd.mour.android
 
+import ac.dnd.mour.android.domain.repository.AuthenticationRepository
 import ac.dnd.mour.android.presentation.common.CHANNEL_1
 import ac.dnd.mour.android.presentation.common.CHANNEL_GROUP_1
+import ac.dnd.mour.android.presentation.ui.invalid.InvalidJwtTokenActivity
 import android.app.Application
 import android.app.NotificationChannel
 import android.app.NotificationChannelGroup
 import android.app.NotificationManager
 import android.content.Context
+import android.content.Intent
 import android.os.Build
 import androidx.annotation.RequiresApi
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.ProcessLifecycleOwner
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.crashlytics.ktx.crashlytics
 import com.google.firebase.ktx.Firebase
 import com.kakao.sdk.common.KakaoSdk
 import dagger.hilt.android.HiltAndroidApp
+import io.sentry.Sentry
+import javax.inject.Inject
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.launch
+import timber.log.Timber
 
 @HiltAndroidApp
 open class MourApplication : Application() {
+
+    @Inject
+    lateinit var authenticationRepository: AuthenticationRepository
+
+    private val handler = CoroutineExceptionHandler { _, exception ->
+        Timber.d(exception)
+        Sentry.captureException(exception)
+        Firebase.crashlytics.recordException(exception)
+    }
+
     override fun onCreate() {
         super.onCreate()
         KakaoSdk.init(this, BuildConfig.KAKAO_APP_KEY)
@@ -24,8 +47,8 @@ open class MourApplication : Application() {
             initializeChannel(this)
         }
 
-        // Firebase Initialize
-        Firebase.analytics
+        initializeFirebase()
+        observeRefreshTokenValidation()
     }
 
     @RequiresApi(Build.VERSION_CODES.O)
@@ -50,6 +73,31 @@ open class MourApplication : Application() {
         context.getSystemService(NotificationManager::class.java).run {
             createNotificationChannel(channel1)
             createNotificationChannelGroup(group)
+        }
+    }
+
+    private fun initializeFirebase() {
+        Firebase.analytics
+    }
+
+    private fun observeRefreshTokenValidation() {
+        with(ProcessLifecycleOwner.get()) {
+            lifecycleScope.launch(handler) {
+                repeatOnLifecycle(Lifecycle.State.STARTED) {
+                    authenticationRepository.isRefreshTokenInvalid.collect { isRefreshTokenInvalid ->
+                        if (isRefreshTokenInvalid) {
+                            val intent = Intent(
+                                this@MourApplication,
+                                InvalidJwtTokenActivity::class.java
+                            ).apply {
+                                flags =
+                                    Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                            }
+                            startActivity(intent)
+                        }
+                    }
+                }
+            }
         }
     }
 }
---
+++ presentation/src/main/kotlin/ac/dnd/mour/android/presentation/ui/invalid/InvalidJwtTokenViewModel.kt
@@ -0,0 +1,8 @@
+package ac.dnd.mour.android.presentation.ui.invalid
+
+import ac.dnd.mour.android.presentation.common.base.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class InvalidJwtTokenViewModel @Inject constructor() : BaseViewModel()
