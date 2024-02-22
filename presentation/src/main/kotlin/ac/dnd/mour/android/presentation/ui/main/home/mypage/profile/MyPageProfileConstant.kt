7bd8fa0217605c72697b252a3682967f20647fa2
김진우 <85734140+jinuemong@users.noreply.github.com>
2024-02-22 20:37:04 +0900

    [Fix]: 마이페이지 오류 수정 (#118)

--- presentation/src/main/kotlin/ac/dnd/mour/android/presentation/ui/main/home/mypage/profile/MyPageProfileConstant.kt
+++ presentation/src/main/kotlin/ac/dnd/mour/android/presentation/ui/main/home/mypage/profile/MyPageProfileConstant.kt
@@ -4,7 +4,4 @@ import ac.dnd.mour.android.presentation.ui.main.home.mypage.MyPageConstant
 
 object MyPageProfileConstant {
     const val ROUTE: String = "${MyPageConstant.ROUTE}/profile"
-
-    const val ROURE_ARGUMENT_USER_MODEL = "profileModel"
-    const val CONTAIN_USER_MODEL = "${ROUTE}/{${ROURE_ARGUMENT_USER_MODEL}}"
 }
--- presentation/src/main/kotlin/ac/dnd/mour/android/presentation/ui/main/home/mypage/profile/MyPageProfileDestination.kt
+++ presentation/src/main/kotlin/ac/dnd/mour/android/presentation/ui/main/home/mypage/profile/MyPageProfileDestination.kt
@@ -1,42 +1,28 @@
 package ac.dnd.mour.android.presentation.ui.main.home.mypage.profile
 
 import ac.dnd.mour.android.presentation.common.util.ErrorObserver
-import ac.dnd.mour.android.presentation.model.mypage.ProfileModel
 import ac.dnd.mour.android.presentation.ui.main.ApplicationState
 import androidx.compose.runtime.getValue
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
-import kotlinx.datetime.LocalDate
 
 fun NavGraphBuilder.myPageProfileDestination(
     appState: ApplicationState
 ) {
     composable(
-        route = MyPageProfileConstant.CONTAIN_USER_MODEL
+        route = MyPageProfileConstant.ROUTE
     ) {
-
-        val profileModel = appState.navController.previousBackStackEntry
-            ?.savedStateHandle
-            ?.get<ProfileModel>(MyPageProfileConstant.ROURE_ARGUMENT_USER_MODEL)
-            ?: ProfileModel(
-                id = 0L,
-                email = "",
-                profileImageUrl = "",
-                name = "",
-                nickname = "",
-                gender = "",
-                birth = LocalDate(2020, 1, 1)
-            )
 
         val viewModel: MyPageProfileViewModel = hiltViewModel()
 
         val model: MyPageProfileModel = let {
             val state by viewModel.state.collectAsStateWithLifecycle()
+            val profile by viewModel.profile.collectAsStateWithLifecycle()
             MyPageProfileModel(
                 state = state,
-                profile = profileModel
+                profile = profile
             )
         }
 
