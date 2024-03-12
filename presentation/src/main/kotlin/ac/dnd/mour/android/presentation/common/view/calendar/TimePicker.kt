7b910f8dbfe681cf382ab93c67c2be8198e35f6d
김진우 <jinwoo3661@naver.com>
2024-03-12 19:38:37 +0900

    [fix] timePicker 수정

--- presentation/src/main/kotlin/ac/dnd/mour/android/presentation/common/view/calendar/TimePicker.kt
+++ presentation/src/main/kotlin/ac/dnd/mour/android/presentation/common/view/calendar/TimePicker.kt
@@ -1,6 +1,8 @@
 package ac.dnd.mour.android.presentation.common.view.calendar
 
+import ac.dnd.mour.android.presentation.common.theme.Body0
 import ac.dnd.mour.android.presentation.common.theme.Gray200
+import ac.dnd.mour.android.presentation.common.theme.Gray500
 import ac.dnd.mour.android.presentation.common.theme.Gray700
 import ac.dnd.mour.android.presentation.common.theme.Gray800
 import ac.dnd.mour.android.presentation.common.theme.Headline2
@@ -16,8 +18,12 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.material.RadioButton
+import androidx.compose.material.RadioButtonDefaults
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -40,8 +46,9 @@ fun TimePicker(
     localTime: LocalTime = LocalTime(0, 0),
     properties: BottomSheetDialogProperties = BottomSheetDialogProperties(),
     onDismissRequest: () -> Unit,
-    onConfirm: (LocalTime) -> Unit
-) {
+    onConfirm: (LocalTime) -> Unit,
+
+    ) {
     val timeTextStyle = Headline3.merge(
         color = Gray700,
         fontWeight = FontWeight.SemiBold
@@ -57,14 +64,13 @@ fun TimePicker(
         )
     }
     var minuteValue by remember { mutableIntStateOf(localTime.minute) }
+    var isCheckNotValue by remember { mutableStateOf(false) }
 
     BottomSheetScreen(
         onDismissRequest = onDismissRequest,
         properties = properties
     ) {
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
+        Column {
             Box(
                 modifier = Modifier
                     .background(Gray200)
@@ -98,10 +104,14 @@ fun TimePicker(
                                 TimePickerType.PM -> if (hourValue == 12) 0 else 12
                             }
                             onConfirm(
-                                LocalTime(
-                                    hour = hourValue + transHour,
-                                    minute = minuteValue
-                                )
+                                if (isCheckNotValue) {
+                                    LocalTime(0, 0)
+                                } else {
+                                    LocalTime(
+                                        hour = hourValue + transHour,
+                                        minute = minuteValue
+                                    )
+                                }
                             )
                             onDismissRequest()
                         }
@@ -109,28 +119,9 @@ fun TimePicker(
             }
             Row(
                 verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.Center
+                horizontalArrangement = Arrangement.Center,
+                modifier = Modifier.fillMaxWidth()
             ) {
-                NumberPicker(
-                    value = hourValue,
-                    range = 1..12,
-                    dividersColor = Primary3,
-                    textStyle = timeTextStyle,
-                    onValueChange = {
-                        hourValue = it
-                    },
-                )
-                Spacer(modifier = Modifier.width(20.dp))
-                NumberPicker(
-                    value = minuteValue,
-                    range = 0..59,
-                    dividersColor = Primary3,
-                    textStyle = timeTextStyle,
-                    onValueChange = {
-                        minuteValue = it
-                    },
-                )
-                Spacer(modifier = Modifier.width(20.dp))
                 ListItemPicker(
                     value = timeType.typeName,
                     list = TimePickerType.getEntryNames(),
@@ -140,7 +131,65 @@ fun TimePicker(
                     dividersColor = Primary3,
                     textStyle = timeTextStyle
                 )
+                Spacer(modifier = Modifier.width(40.dp))
+                NumberPicker(
+                    value = hourValue,
+                    range = 1..12,
+                    dividersColor = Primary3,
+                    textStyle = timeTextStyle,
+                    onValueChange = {
+                        hourValue = it
+                    },
+                )
+                Spacer(modifier = Modifier.width(10.dp))
+                Text(
+                    text = "시",
+                    style = Body0.merge(
+                        color = Gray800,
+                        fontWeight = FontWeight.Normal
+                    ),
+                )
+                Spacer(modifier = Modifier.width(26.dp))
+                NumberPicker(
+                    value = minuteValue,
+                    range = 0..59,
+                    dividersColor = Primary3,
+                    textStyle = timeTextStyle,
+                    onValueChange = {
+                        minuteValue = it
+                    },
+                )
+                Text(
+                    text = "분",
+                    fontWeight = FontWeight.Normal,
+                    style = Body0.merge(color = Gray800),
+                )
             }
+
+            Spacer(modifier = Modifier.height(35.dp))
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Spacer(modifier = Modifier.width(22.dp))
+                RadioButton(
+                    selected = isCheckNotValue,
+                    onClick = {
+                        isCheckNotValue = !isCheckNotValue
+                    },
+                    modifier = Modifier.size(16.dp),
+                    colors = RadioButtonDefaults.colors(
+                        selectedColor = Primary4,
+                        unselectedColor = Gray500
+                    )
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+                Text(
+                    text = "선택 안함",
+                    fontWeight = FontWeight.Normal,
+                    style = Body0.merge(color = Gray800),
+                )
+            }
+            Spacer(modifier = Modifier.height(28.dp))
         }
     }
 }
